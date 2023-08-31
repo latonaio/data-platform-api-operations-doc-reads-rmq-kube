18b3015c28ea6b2613bf87d3cd5b0885d82de87f
misupopo <kokorononakaniitumo@yahoo.co.jp>
2023-08-31 17:05:33 +0900

    no message

---
+++ DPFM_API_Caller/requests/item_doc.go
@@ -0,0 +1,13 @@
+package requests
+
+type ItemDoc struct {
+	Operations			 	 int	`json:"Operations"`
+	OperationsItem	         int	`json:"OperationsItem"`
+	DocType                  string `json:"DocType"`
+	DocVersionID             int    `json:"DocVersionID"`
+	DocID                    string `json:"DocID"`
+	FileExtension            string `json:"FileExtension"`
+	FileName                 string `json:"FileName"`
+	FilePath                 string `json:"FilePath"`
+	DocIssuerBusinessPartner int    `json:"DocIssuerBusinessPartner"`
+}
